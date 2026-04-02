b37e434df128ea93ad7d56b7b60c9871145d942d
Mahtab645 <ansarimehtab645@gmail.com>
2026-04-02 21:41:36 +0530

    contact us done

--- contact.php
+++ contact.php
@@ -9,17 +9,74 @@
     </div>
 </section>
 
-<!-- Coming Soon Section -->
-<section class="coming-soon">
+<!-- Contact Section -->
+<section class="contact-section">
     <div class="container">
-        <div class="coming-soon-content">
-            <h2>Coming Soon</h2>
-            <p>We're working hard to bring you amazing content. Stay tuned!</p>
-            <div class="coming-soon-icon">
-                <i class="fas fa-tools"></i>
+        
+        <div class="contact-content">
+            <div class="row">
+                <!-- Contact Details -->
+                <div class="col-lg-5 col-md-12">
+                    <div class="contact-info">
+                        <h3 class="pb-4">Contact Information</h3>
+                       
+                        <div class="contact-item">
+                            <div class="contact-icon">
+                                <i class="fas fa-map-marker-alt"></i>
+                            </div>
+                            <div class="contact-text">
+                                <h4>Address</h4>
+                                <p>Valnexis Engineering
+                                Mumbai, Maharashtra<br>
+                                India - 400001</p>
+                            </div>
+                        </div>
+                        
+                        <div class="contact-item">
+                            <div class="contact-icon">
+                                <i class="fas fa-phone"></i>
+                            </div>
+                            <div class="contact-text">
+                                <h4>Phone</h4>
+                                <p>+91 90827 83860</p>
+                            </div>
+                        </div>
+                        
+                        <div class="contact-item">
+                            <div class="contact-icon">
+                                <i class="fas fa-envelope"></i>
+                            </div>
+                            <div class="contact-text">
+                                <h4>Email</h4>
+                                <p>sales@valnexis.in</p>
+                            </div>
+                        </div>
+                        
+                      
+                    </div>
+                </div>
+                
+                <!-- Map -->
+                <div class="col-lg-7 col-md-12 ps-5">
+                    <div class="map-container">
+                        <div class="map-wrapper">
+                            <iframe 
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.744632845!2d72.8324!3d19.0760!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTnCsDA0JzMzLjYiTiA3MsKwNDknNTYuNCJF!5e0!3m2!1sen!2sin!4v1234567890"
+                                width="100%" 
+                                height="450" 
+                                style="border:0;" 
+                                allowfullscreen="" 
+                                loading="lazy">
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </section>
+
+<!-- Quick Contact Form -->
+
 
 <?php include 'footer.php'; ?>
